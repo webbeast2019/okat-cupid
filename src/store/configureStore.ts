e94569a6e5aeffad471a86a8fb63a414ed6e65c8
yuvalbl <yuval.bar.levi@gmail.com>
2019-06-19 13:39:16 +0300

    add redux devtools

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,15 +1,19 @@
-import {combineReducers, createStore} from 'redux'
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import catsDataReducer from './cats.data.reducer';
 import {Cat} from '../models/Cat';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 export interface IRootState {
   cats: Array<Cat>
 }
 
 export default function configureStore() {
+  const middlewareEnhancer = applyMiddleware(/*middleware will be added here*/);
+  const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+  
   const rootReducer = combineReducers({
     cats: catsDataReducer
   });
   
-  return createStore(rootReducer, {});
+  return createStore(rootReducer, {}, composedEnhancers);
 }
