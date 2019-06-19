e3f6df587c5e2ffda0a6097389ab4d10e6b6c6cf
yuvalbl <yuval.bar.levi@gmail.com>
2019-06-19 17:13:34 +0300

    add redux devtools

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC<IProps> = ({getData}) => {
   
   // get data on app init
   useEffect(() => {
-    getData()
+    getData();
   });
   
   return (
--- src/store/cats.api.middleware.ts
+++ src/store/cats.api.middleware.ts
@@ -3,23 +3,23 @@ import {Cat} from '../models/Cat';
 import {catsDataSet} from './cats.data.reducer';
 
 
-const GET_CATS = '[CATS API] GET_CATS';
+const CATS_API_GET_CATS = '[CATS API] GET_CATS';
 
 // actions types
 interface getCatsAction {
-  type: typeof GET_CATS;
+  type: typeof CATS_API_GET_CATS;
 }
 
 // actions factories
 export const getCats = (): getCatsAction => {
   return {
-    type: GET_CATS,
+    type: CATS_API_GET_CATS,
   }
 };
 
 // see: https://stackoverflow.com/questions/45339448/how-do-you-create-strongly-typed-redux-middleware-in-typescript-from-reduxs-typ
 const getCatsFlow:Middleware = ({dispatch}: MiddlewareAPI) => (next: Dispatch) => action => {
-  if (action.type === GET_CATS) {
+  if (action.type === CATS_API_GET_CATS) {
     // see: https://facebook.github.io/create-react-app/docs/deployment#github-pages-https-pagesgithubcom
     // https://facebook.github.io/create-react-app/docs/using-the-public-folder
     fetch(process.env.PUBLIC_URL + '/data/cats.json')
