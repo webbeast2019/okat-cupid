7bfdafc33bd474a92a44a793706a7ea7e4a0710e
yuvalbl <yuval.bar.levi@gmail.com>
2019-06-26 20:41:57 +0300

    step 09

--- src/components/ProfileCard/ProfileCard.test.tsx
+++ src/components/ProfileCard/ProfileCard.test.tsx
@@ -4,9 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import ProfileCard from './ProfileCard';
 import {Link as RouterLink} from 'react-router-dom';
 
-configure({adapter: new Adapter})
+configure({adapter: new Adapter});
 
-describe('<Profile/>', () => {
+describe('<ProfileCard/>', () => {
   it('should render without a RouterLink element', () => {
     const wrapper = shallow(<ProfileCard imgSrc={`${process.env.PUBLIC_URL}/img/cat-default.jpg`}
                                          name="Testy cat"
@@ -15,20 +15,3 @@ describe('<Profile/>', () => {
     // expect(wrapper.contains(RouterLink)).toEqual(true);
   })
 });
-
-// 1. add test for ProfileCard short
-// 2. add beforeEach
-
-// let wrapper;
-// beforeEach(() => {
-//   wrapper = <ProfileCard imgSrc={`${process.env.PUBLIC_URL}/img/cat-default.jpg`}
-//                          name="Testy cat"
-//                          description="desc"/>
-// });
-
-// 3. use wrapper.setProps({short: true})
-
-
-
-// expect(wrapper.find('img.profile-image-small').length).toEqual(0);
-// expect(wrapper.find('img.profile-image-large').length).toEqual(1);
