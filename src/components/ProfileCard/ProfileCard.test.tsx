6ecee39c1b6e23f792792e73eae534fd4bab2909
yuvalbl <yuval.bar.levi@gmail.com>
2019-06-27 11:46:03 +0300

    step 09

--- src/components/ProfileCard/ProfileCard.test.tsx
+++ src/components/ProfileCard/ProfileCard.test.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {configure, shallow} from 'enzyme';
+import {configure, shallow as EnzymeShallow, ShallowWrapper} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ProfileCard from './ProfileCard';
 import {Link as RouterLink} from 'react-router-dom';
+import {createShallow} from '@material-ui/core/test-utils';
 
 configure({adapter: new Adapter});
 
 describe('<ProfileCard/>', () => {
-  it('should render without a RouterLink element', () => {
-    const wrapper = shallow(<ProfileCard imgSrc={`${process.env.PUBLIC_URL}/img/cat-default.jpg`}
-                                         name="Testy cat"
-                                         description="desc"/>);
+  let shallow: typeof EnzymeShallow;
+  let wrapper: ShallowWrapper;
+  
+  beforeAll(() => {  // This is Mocha; in Jest, use beforeAll
+    shallow = createShallow();
+  });
+  
+  beforeEach(() => {
+    wrapper = shallow(<ProfileCard imgSrc={`${process.env.PUBLIC_URL}/img/cat-default.jpg`}
+                                   id={'cat-id'}
+                                   name="Testy cat"
+                                   description="desc"/>)
+  });
+  
+  it('should render without RouterLink element', () => {
     expect(wrapper.find(RouterLink).length).toEqual(0);
-    // expect(wrapper.contains(RouterLink)).toEqual(true);
+  });
+  
+  it('should render with 1 RouterLink element', () => {
+    wrapper.setProps({short: true});
+    expect(wrapper.find(RouterLink).length).toEqual(1);
   })
 });
+
+
+// 3. use wrapper.setProps({short: true})
+// expect(wrapper.find('img.profile-image-small').length).toEqual(0);
+// expect(wrapper.find('img.profile-image-large').length).toEqual(1);
